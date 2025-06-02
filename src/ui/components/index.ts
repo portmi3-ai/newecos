bb56294ac8961c03c79a1586d298fb7c14a2adba
Michael Porter <michaelport3@gmail.com>
2025-06-01 23:30:11 -0400

    Install Firebase CLI and fix npm scripts

--- src/ui/components/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all components from one file for easier imports
-export { default as Avatar } from './Avatar';
-export { default as Badge } from './Badge';
-export { default as Breadcrumbs } from './Breadcrumbs';
-export { default as Modal } from './Modal';
-export { default as Skeleton, TextSkeleton, CardSkeleton } from './Skeleton';
-export { default as Tabs } from './Tabs';
-export { default as Tooltip } from './Tooltip';
